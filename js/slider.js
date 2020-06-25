ff64d55bf83875cc30a2ee9761bb12d8d6c76027
Asilbek <66477122+asilbekkod@users.noreply.github.com>
2020-06-25 19:49:13 +0500

    Переработан Слайдер

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+})
