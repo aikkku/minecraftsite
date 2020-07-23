495518a6f3c3063cdf367c19e4e7e7db3283eeb3
Asilbek <66477122+asilbekkod@users.noreply.github.com>
2020-07-23 14:15:42 +0500

    Полностью отработана мобильная версия и земенен слайдер

--- js/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        arrows: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
-})
